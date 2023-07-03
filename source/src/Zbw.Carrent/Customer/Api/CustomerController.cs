df966ce02f687a7c5183e2651ea09f2f92b6375f
saschafehr <94016338+saschafehr@users.noreply.github.com>
2023-07-03 18:08:29 +0200

    added yaml file

---
+++ source/src/Zbw.Carrent/Customer/Api/CustomerController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Zbw.Carrent.Customer.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        // GET: api/<CustomerController>
+        [HttpGet]
+        public IEnumerable<CustomerResponse> Get()
+        {
+            yield return new CustomerResponse
+            {
+                Id = Guid.NewGuid(),
+                Name = "Sascha",
+                CustomerNr = "S1",
+                Address = "Ilgenstrasse 21"
+            };
+        }
+
+        // GET api/<CustomerController>/5
+        [HttpGet("{id}")]
+        public string Get(Guid id)
+        {
+            return "value";
+        }
+
+        // POST api/<CustomerController>
+        [HttpPost]
+        public void Post([FromBody] CustomerRequest value)
+        {
+        }
+
+        // PUT api/<CustomerController>/5
+        [HttpPut("{id}")]
+        public void Put(Guid id, [FromBody] CustomerRequest value)
+        {
+        }
+
+        // DELETE api/<CustomerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(Guid id)
+        {
+        }
+    }
+}
---
+++ source/src/Zbw.Carrent/Customer/Api/CustomerRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Zbw.Carrent.Customer.Api
+{
+    public class CustomerRequest
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
